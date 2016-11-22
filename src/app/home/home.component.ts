022ebf1a2d924937ada119701b83408faa5e2d63
iidxmania <tonlnwforever@gmail.com>
2016-11-22 09:37:13 +0700

    update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CarService } from '../services/car.service';
-import { Car } from '../../Car';
 
 @Component({
   selector: 'home',
@@ -12,10 +10,5 @@ export class HomeComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-<<<<<<< HEAD
-        
-=======
-    
->>>>>>> 57717a47d60455658068fe856d4d63c26d17d12f
     }
 }
