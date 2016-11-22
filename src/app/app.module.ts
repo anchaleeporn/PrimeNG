6ff8d2f2f44987a45242409c5cc3e3cd1c304915
anchaleeporn <life_freedom_to-play7088@hotmail.com>
2016-11-22 10:57:19 +0700

    edit button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import {InputMaskModule} from 'primeng/primeng';
-import {InputTextModule} from 'primeng/primeng';
-import {AutoCompleteModule} from 'primeng/primeng';
-import {DataTableModule,SharedModule} from 'primeng/primeng';
+import {InputTextModule,DataTableModule,SharedModule,ButtonModule} from 'primeng/primeng';
 
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
@@ -22,10 +19,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule,
     InputTextModule,
-    InputMaskModule,
-    AutoCompleteModule,
     DataTableModule,
-    SharedModule
+    SharedModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,9 +8,9 @@ import { Car } from '../../Car';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+    text: string;
     constructor() { }
-
+    onclick() {}
     ngOnInit() {
     
     }
